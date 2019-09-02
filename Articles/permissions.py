03b65ea3b8ef1d55c5addce572d422f5a6c4f1d4
banjac90 <banjac90@gmail.com>
2019-09-02 02:50:32 +0200

    Update permissions.py

--- Articles/permissions.py
+++ Articles/permissions.py
@@ -2,15 +2,18 @@ from rest_framework import permissions
 
 class IsOwnerOrReadOnly(permissions.BasePermission):
     """
-    Object-level permission to only allow owners of an object to edit it.
-    Assumes the model instance has an `owner` attribute.
-    """
+    Custom permission to only allow owners of an object to edit it.
+    """ 
 
     def has_object_permission(self, request, view, obj):
         # Read permissions are allowed to any request,
         # so we'll always allow GET, HEAD or OPTIONS requests.
-        if request.method in permissions.SAFE_METHODS:
-            return True
-
-        # Instance must have an attribute named `owner`.
+        if not obj.stream:
+            return False
+        obj = obj.stream
+        if not request.user.is_anonymous() and request.method in permissions.SAFE_METHODS:
+            members = obj.members.filter(user=request.user)
+            if len(members):
+                return True
+        # Write permissions are only allowed to the owner of the snippet.
         return obj.owner == request.user
