fa6871aba9ee047c38513753fc73d7916e1af97c
banjac90 <banjac90@gmail.com>
2019-09-02 02:56:06 +0200

    Update models.py

--- Articles/models.py
+++ Articles/models.py
@@ -1,6 +1,6 @@
 from __future__ import unicode_literals
 from django.db import models
-from Users.models import User
+from django.conf import settings
 from rest_framework.reverse import reverse as api_reverse
 
 class Category(models.Model):
@@ -11,11 +11,11 @@ class Category(models.Model):
 
 class Article(models.Model):
 	category = models.ForeignKey(Category, on_delete=models.CASCADE)
-	created_by_user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+	created_by_user_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 	title = models.CharField(max_length=100)
 	content = models.TextField()
-	created_at = models.DateTimeField(auto_now_add=True, auto_now=False)
-	update_at = models.DateTimeField(auto_now=True, auto_now_add=False)
+	created_at = models.DateTimeField(auto_now_add=True)
+	update_at = models.DateTimeField(auto_now=True)
 
 	def __str__(self):
 		return self.title
@@ -23,6 +23,3 @@ class Article(models.Model):
 	@property
 	def owner(self):
 		return self.created_by_user_id
-
-	
-
