08bd24f7b5d434088ef6d69d6b4aab1a3eda1c9c
banjac90 <banjac90@gmail.com>
2019-09-02 02:48:59 +0200

    Update views.py

--- Users/views.py
+++ Users/views.py
@@ -1,10 +1,15 @@
-from .serializers import *
-from rest_framework import status
+from .serializers import UserSerializer
 from rest_framework.permissions import AllowAny
-from rest_framework.response import Response
 from rest_framework.generics import CreateAPIView
+from rest_framework import status
+from rest_framework.response import Response
 
 class RegisterUserAPIView(CreateAPIView):    
     permission_classes = (AllowAny,) 
-    serializer_class = RegisterationUserSerializer
+    serializer_class = UserSerializer
 
+    def post(self, request):
+    	serializer = self.serializer_class(data=request.data)
+    	if serializer.is_valid():
+    		serializer.save()
+    		return Response(serializer.data, status=status.HTTP_201_CREATED)
