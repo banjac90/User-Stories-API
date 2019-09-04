a2727c1aa501bdceefffff8e347db389d012d62c
banjac90 <banjac90@gmail.com>
2019-09-04 18:38:16 +0200

    Update urls.py

--- UserStories/urls.py
+++ UserStories/urls.py
@@ -7,27 +7,27 @@ from rest_framework.schemas import get_schema_view
 
 
 urlpatterns = [
-	
-
     path('admin/', admin.site.urls),
     path('article-api/', include('Articles.urls', namespace='articles-api')),
-    path('Users/', include('Users.urls', namespace='users')),
-    path('api-auth/', include('rest_framework.urls')),
-
-    #login with jwt token, refresh token and verify token
-    path('api/token/',TokenObtainPairView.as_view()),
-    path('api/token/refresh/', TokenRefreshView.as_view()),
-    path('api/token/verify/', TokenRefreshView.as_view()),
+    path('Users/', include('Users.urls', namespace='users')), 
     
-    path('openapi', get_schema_view(
+    
+    #JWT token
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    #password reset
+    path('pass-reset/', include('django_rest_passwordreset.urls', namespace='password_reset')),
+
+    #Swagger documentations
+    path('api-auth/', include('rest_framework.urls')),
+    path('openapi/', get_schema_view(
         title="User Stories API",
         description="API for User Stories"        
     ), name='openapi-schema'),
-
-
-    path('swagger-ui/',TemplateView.as_view(
+    path('',TemplateView.as_view(
     	template_name='swagger-ui.html',
     	extra_context = {'schema_url':'openapi-schema'}
-    	), name='swagger-ui')
+    	), name='swagger-ui'),
 
 ]
